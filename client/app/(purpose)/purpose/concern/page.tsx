fb2b1c931b67d7cc1d11b452143393f3150dcb36
Drakkarrr <junrey404@gmail.com>
2023-12-06 18:52:20 +0800

    bug: fixed the print ticket bug target

--- client/app/(purpose)/purpose/concern/page.tsx
+++ client/app/(purpose)/purpose/concern/page.tsx
@@ -1,14 +1,15 @@
-"use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import Logo from "@/assets/logo.png";
-import BgWaves from "@/assets/bg-waves.png";
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { useCallback } from "react";
-import useRequest from "@/hooks/useRequest";
-import React, { useState, useEffect } from 'react';
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Logo from '@/assets/logo.png';
+import BgWaves from '@/assets/bg-waves.png';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { useCallback, useEffect } from 'react'; // Import useEffect
+import useRequest from '@/hooks/useRequest';
+import React, { useState } from 'react'; // Remove the import for useEffect (already imported)
+
 import { getQueue } from '@/app/api/api';
 
 export default function ConcernPage() {
@@ -26,76 +27,102 @@ export default function ConcernPage() {
     }
   };
 
+  useEffect(() => {
+    // Add the iframe dynamically when the component mounts
+    const iframe = document.createElement('iframe');
+    iframe.id = 'print_frame';
+    iframe.name = 'print_frame';
+    iframe.width = '0';
+    iframe.height = '0';
+    iframe.frameBorder = '0';
+    iframe.src = 'about:blank';
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+
+    return () => {
+      // Remove the iframe when the component unmounts
+      document.body.removeChild(iframe);
+    };
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
   const handlePrintQueue = async () => {
-    const res = await getQueue({
-      que_type: 'concern'
-    });
+    try {
+      const res = await getQueue({
+        que_type: 'concern',
+      });
 
-    if (res.status == 200) {
-      console.log('TICKET RESPONSE: ', res.data);
+      if (res.status === 200) {
+        console.log('TICKET RESPONSE: ', res.data);
 
-      // Create a new window for printing
-      const printWindow = window.open('', '_blank');
-      printWindow!.document.write(`<html><head><title>Print Q. number</title></head><body>`);
-      printWindow!.document.write(`<div>Type Of Transaction: ${res.data['queue_item']['que_type']}</div>`); // Customize with your actual queue number
-      printWindow!.document.write(`<h1>${res.data['queue_item']['priority_number']}</h1>`); // Customize with your actual queue number
-      printWindow!.document.write(`<div>Estimated Time: ${res.data['estimated_waiting_time']}</div>`); // Customize with your actual queue number
-      printWindow!.document.write(`</body></html>`);
-      printWindow!.document.close(); // Necessary for IE >= 10
-      printWindow!.focus(); // Necessary for IE >= 10
-      // Use onfocus event to close the window after printing
-      printWindow!.onfocus = () => {
-        setTimeout(() => {
-          printWindow!.close();
-        }, 100); // Use a delay to ensure the print dialog has closed
-      };
+        // Prepare the content for printing with updated styles
+        const printContent = `
+          <style>
+            body { margin: 0; padding: 0; text-align: center; }
+            h1 { font-size: 2em; color: red; }
+            div { margin: 10px; }
+          </style>
+          <div>Type Of Transaction: ${res.data['queue_item']['que_type']}</div>
+          <h1>${res.data['queue_item']['priority_number']}</h1>
+          <div>Estimated Waiting Time: ${res.data['estimated_waiting_time']} minutes</div>
+        `;
 
-      printWindow!.print();
-      router.push("/thankyou");
+        // Create a new HTML document for printing
+        const printDocument =
+          document.implementation.createHTMLDocument('Print Document');
+        printDocument.body.innerHTML = printContent;
+
+        // Print the content
+        window.frames['print_frame' as any].document.body.innerHTML =
+          printDocument.body.innerHTML;
+        window.frames['print_frame' as any].window.focus();
+        window.frames['print_frame' as any].window.print();
+
+        // Redirect to the "/thankyou" page
+        router.push('/thankyou');
+      }
+    } catch (error) {
+      console.error('Error fetching/printing queue data:', error);
     }
   };
 
   return (
-    <div className="grid w-full h-full place-items-center">
-      <div className="relative w-full h-60">
+    <div className='grid w-full h-full place-items-center'>
+      <div className='relative w-full h-60'>
         <Image
           src={BgWaves}
-          layout="fill"
-          objectPosition="bottom"
-          alt="Moelci-II waves"
-          className="rotate-180"
+          layout='fill'
+          objectPosition='bottom'
+          alt='Moelci-II waves'
+          className='rotate-180'
         />
       </div>
-      <div className="grid w-10/12 p-8 space-y-10 ring ring-black ring-offset-2 ring-offset-slate-50 dark:ring-offset-slate-900 place-items-center">
+      <div className='grid w-10/12 p-8 space-y-10 ring ring-black ring-offset-2 ring-offset-slate-50 dark:ring-offset-slate-900 place-items-center'>
         <Image
           src={Logo}
           width={200}
           height={200}
-          alt="Moelci-II logo"
-          className="absolute top-32"
+          alt='Moelci-II logo'
+          className='absolute top-32'
         />
-        <div className="w-full space-y-32 text-center">
-          <h2 className="font-heading text-3xl font-bold bg-[#F9D029] w-96 py-4 m-auto rounded-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+        <div className='w-full space-y-32 text-center'>
+          <h2 className='font-heading text-3xl font-bold bg-[#F9D029] w-96 py-4 m-auto rounded-3xl leading-[1.1] sm:text-3xl md:text-6xl'>
             Concern
           </h2>
-          <div className="flex justify-around w-full">
+          <div className='flex justify-around w-full'>
             <Link
-              href="#"
+              href='#'
               onClick={() => router.back()}
               className={`rounded-none text-xl tracking-wide font-bold bg-black hover:pb-2 text-white ${cn(
-                buttonVariants({ size: "lg" }),
-              )}`}
-            >
+                buttonVariants({ size: 'lg' })
+              )}`}>
               &larr; Back
             </Link>
             <Link
-              // href="/thankyou"
-              href="#"
+              href='#'
               onClick={handlePrintQueue}
               className={`rounded-none text-xl tracking-wide font-bold hover:pb-2 text-white ${cn(
-                buttonVariants({ size: "lg", variant: "destructive" }),
-              )}`}
-            >
+                buttonVariants({ size: 'lg', variant: 'destructive' })
+              )}`}>
               Print Q. number
             </Link>
           </div>
---
+++ client/components/ui/iframe.tsx
@@ -0,0 +1,10 @@
+// Add this somewhere in your JSX file, preferably in the head of the document.
+<iframe
+  id='print_frame'
+  name='print_frame'
+  width='0'
+  height='0'
+  frameBorder='0'
+  src='about:blank'
+  style={{ display: 'none' }} // This will hide the iframe
+></iframe>;
