7de02627ea3dc248a67b1f92aa8418e1fee8dd45
Drakkarrr <junrey404@gmail.com>
2023-12-06 18:10:56 +0800

    changed api enpoint to actual

--- client/app/api/api.js
+++ client/app/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://server.test/api'; // Replace with your Laravel API URL
+const API_URL = 'http://localhost:8000/api';
 
 // Function to api_url
 export const server_url = async () => {
@@ -16,68 +16,67 @@ export const login = async (credentials) => {
 export const getMe = async (token) => {
   return axios.get(`${API_URL}/me`, {
     headers: {
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 // Function to fetch users
 export const getUsers = async (token) => {
-    return axios.get(`${API_URL}/users`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-  };
-  
+  return axios.get(`${API_URL}/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
 // Function to update a user
 export const updateUser = async (userId, userData, token) => {
-    return axios.put(`${API_URL}/users/${userId}`, userData, {
-        headers: {
-        Authorization: `Bearer ${token}`
-        }
-    });
+  return axios.put(`${API_URL}/users/${userId}`, userData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
-  
+
 // Function to get the current queue
 export const getQueue = async (data) => {
-    return axios.post(`${API_URL}/queue/get_que`, data, {
-        headers: {
-            Accept: 'application/json',
-            ContentType: 'application/json',
-        }
-    });    
+  return axios.post(`${API_URL}/queue/get_que`, data, {
+    headers: {
+      Accept: 'application/json',
+      ContentType: 'application/json',
+    },
+  });
 };
 
 // Function to get the currently serving queue item
 export const getServing = async () => {
-return axios.get(`${API_URL}/queue/serving`);
+  return axios.get(`${API_URL}/queue/serving`);
 };
 
 // Function to get the next queue item
 export const getNext = async (token) => {
-    return axios.get(`${API_URL}/queue/next`, {
-        headers: {
-        Authorization: `Bearer ${token}`
-        }
-    });
+  return axios.get(`${API_URL}/queue/next`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
-  
+
 // Function to get the previous queue item
 export const getPrevious = async (token) => {
-return axios.get(`${API_URL}/queue/previous`, {
+  return axios.get(`${API_URL}/queue/previous`, {
     headers: {
-    Authorization: `Bearer ${token}`
-    }
-});
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
-  
+
 // Function to reset the queue
 export const resetQueue = async (token) => {
-return axios.get(`${API_URL}/queue/reset`, {
+  return axios.get(`${API_URL}/queue/reset`, {
     headers: {
-    Authorization: `Bearer ${token}`
-    }
-});
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
-  
